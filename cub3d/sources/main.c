7bc0682716dd664d96d6a78526ef020c95a5c7ff
El Mehdi El Garouaz <eel-garo@e3r1p16.1337.ma>
2025-07-25 13:53:43 +0100

    Add texture + big of the code

--- cub3d/sources/main.c
+++ cub3d/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: eel-garo <eel-garo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 20:23:58 by ymazini           #+#    #+#             */
-/*   Updated: 2025/07/25 13:06:21 by eel-garo         ###   ########.fr       */
+/*   Updated: 2025/07/25 13:25:32 by eel-garo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,30 +63,9 @@ void	run_parser(char *filename, t_game *game)
 	validate_walls_are_closed(game);
 }
 
-float	rotation_angle(t_game *game)
-{
-	if (game->map.spawn_side_face == 'E')
-		return (0);
-	else if (game->map.spawn_side_face == 'N')
-		return (1.5 * PI);
-	else if (game->map.spawn_side_face == 'W')
-		return (PI);
-	else if (game->map.spawn_side_face == 'S')
-		return (PI / 2);
-	else
-		return (-1);
-}
 
 bool	launch_game(t_game *game)
 {
-	game->player.x = game->map.map_player_x * TILE_SIZE;
-	game->player.y = game->map.map_player_y * TILE_SIZE;
-	game->player.rotation_angle = rotation_angle(game);
-	game->player.turn_direction = 0;
-	game->player.walk_direction = 0;
-	game->player.strafe_direction = 0;
-	game->player.move_speed = 10;
-	game->player.rotation_speed = 1 * (PI / 180);
 	intialize_mlx(game);
 	return (true);
 }
---
+++ cub3d/sources/rendering/load_textures.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_textures.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-garo <eel-garo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/25 13:28:42 by eel-garo          #+#    #+#             */
+/*   Updated: 2025/07/25 13:38:01 by eel-garo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"../../includes/cub3D.h"
+
+static void	load_one_texture(t_game *game, t_texture *tex, char *path)
+{
+	tex->img_ptr = mlx_xpm_file_to_image(game->mlx, path,
+			&tex->width, &tex->height);
+	if (!tex->img_ptr)
+		exit_with_error("Failed to load texture", game);
+	tex->addr = mlx_get_data_addr(tex->img_ptr, &tex->bpp,
+			&tex->line_len, &tex->endian);
+	if (!tex->addr)
+		exit_with_error("Failed to get texture data addr", game);
+}
+
+void	load_textures(t_game *game)
+{
+	load_one_texture(game, &game->asset_data.textures[0],
+		game->asset_data.north_tex_path);
+	load_one_texture(game, &game->asset_data.textures[1],
+		game->asset_data.south_tex_path);
+	load_one_texture(game, &game->asset_data.textures[2],
+		game->asset_data.west_tex_path);
+	load_one_texture(game, &game->asset_data.textures[3],
+		game->asset_data.east_tex_path);
+}
